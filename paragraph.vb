9451b930d73299f40e54f8b5d716d69cbe65dd1e
bhairavigithub123 <bhairavithakare143@gmail.com>
2022-03-12 19:02:06 +0530

    first commit

---
+++ paragraph.vb
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+<title>Paragraph</title>
+</head>
+
+<body>
+<h1>1.C </h1>
+<p>C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system. <br> By design, C provides constructs that map efficiently to typical machine instructions.  </p>
+
+<h1>2.C++ </h1>
+<p> C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or "C with Classes". </p>
+
+
+<h1>3.Javascript </h1>
+
+<p>JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.<br> Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries.  </p>
+
+<h1>4.Python </h1>
+<p>Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.<br> Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small- and large-scale projects. </p>
+
+<h1>5.Java</h1>
+<p> Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.   </p>
+
+
+</body>
+
+</html>
